d1cf57c08cbc9c2b8ba53ac7412e055ac563eb35
Abdelhakim ZETATI <ABDELHAKIMZETATI@gmail.com>
2024-07-23 11:24:16 +0100

    docs: data1

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
 from ScrapeDay import ScrapeDay
-scrape_day = ScrapeDay(["SETTAT", "TANGER"], "test.csv", "01/08/2024")
+scrape_day = ScrapeDay(['TETOUAN SUPRATOURS', 'RABAT AGDAL', 'RABAT VILLE', 'SAFI', 'SALE', 'SETTAT', 'TANGER'],
+                       "data2.csv", "01/08/2024")
 scrape_day.run()
